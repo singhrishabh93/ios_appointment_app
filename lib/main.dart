503da03de9a08a92ada9a7341256d606af3d9a0c
Rishabh Singh <singhrishabh1670@gmail.com>
2024-05-10 16:15:30 +0530

    Final commit for Android(For Android take pull from this commit)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:ios_appointment_app/firebase_options.dart';
 import 'package:ios_appointment_app/signup.dart';
 import 'package:ios_appointment_app/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
